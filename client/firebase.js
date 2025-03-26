26f559149daf237a670fb69a565cf2355ebf7d86
scantizani <scr041990@gmail.com>
2025-03-25 20:25:37 -0400

    Update firebase.js

--- client/firebase.js
+++ client/firebase.js
@@ -1,1 +1,18 @@
+// client/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-project.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
